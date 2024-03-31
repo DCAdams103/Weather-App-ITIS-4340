a9df6f52ed485065d582b9998dc6ec2fa4522906
Dylan Adams <dylancadams1@gmail.com>
2024-03-31 16:29:44 -0400

    Progress on horizontal scrolling

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import {useState, useRef} from 'react';
+import {motion, useTransform, useScroll} from 'framer-motion';
+import {MdChevronRight, MdChevronLeft} from 'react-icons/md';
 
 function App() {
 
   const [inflate, setInflate] = useState(false);
   const [deflate, setDeflate] = useState(false);
+  const [fade, setFade] = useState(false);
   const [text, setText] = useState('View More');
+  const [selected, setSelected] = useState([]);
 
   function timeout(delay) {
       return new Promise( res => setTimeout(res, delay) );
@@ -16,16 +20,138 @@ function App() {
     
     if(!inflate) {
       setInflate(true);
+      setFade(true);
       setText('View Less');
       setDeflate(false);
     } else {
       setDeflate(true);
       setText('View More')
-      await timeout(1000);
+      await timeout(500);
+      setFade(false)
+      await timeout(500);
       setInflate(false);
+      setDeflate(false);
     }
 
   }
+
+  const cards = [
+
+    {
+  
+      url: "sunset-icon.png",
+  
+      title: "Title 1",
+  
+      id: 1,
+  
+    },
+  
+    {
+  
+      url: "sunset-icon.png",
+  
+      title: "Title 2",
+  
+      id: 2,
+  
+    },
+  
+    {
+  
+      url: "sunset-icon.png",
+  
+      title: "Title 3",
+  
+      id: 3,
+  
+    },
+  
+    {
+  
+      url: "sunset-icon.png",
+  
+      title: "Title 4",
+  
+      id: 4,
+  
+    },
+  
+    {
+  
+      url: "sunset-icon.png",
+  
+      title: "Title 5",
+  
+      id: 5,
+  
+    },
+  
+    {
+  
+      url: "sunset-icon.png",
+  
+      title: "Title 6",
+  
+      id: 6,
+  
+    },
+  
+    {
+  
+      url: "sunset-icon.png",
+  
+      title: "Title 7",
+  
+      id: 7,
+  
+    },
+    {
+  
+      url: "sunset-icon.png",
+  
+      title: "Title 8",
+  
+      id: 8,
+  
+    },
+    {
+  
+      url: "sunset-icon.png",
+  
+      title: "Title 9",
+  
+      id: 9,
+  
+    },
+    {
+  
+      url: "sunset-icon.png",
+  
+      title: "Title 9",
+  
+      id: 10,
+  
+    },
+  ];
+
+  const slideLeft = () => {
+    var slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft - 200;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft + 200;
+  };
+
+  const wheelScroll = (e) => {
+    if(e.deltaY > 0) {
+      slideRight();
+    } else {
+      slideLeft();
+    }
+  };
 
   return (
     <>
@@ -47,6 +173,7 @@ function App() {
                 <img src='skywatch-logo.png' className="logo" alt="logo" />
                 <h1 className='title'>SkyWatch</h1>
               </div>
+
             </div>
             
             {/* ------------ Middle Column ------------ */}
@@ -72,18 +199,29 @@ function App() {
                   </div>
                   
                 </div>
+
+                <div className="relative flex items-center">
+                  <MdChevronLeft className="opacity-50 cursor-pointer hover:opacity-100" size={40} onClick={slideLeft}  />
+                  <div id="slider" className="w-full h-full overflow-x-scroll scroll overscroll-none whitespace-nowrap scroll-smooth scrollbar-hide" onWheel={(e) => wheelScroll(e)}>
+                    {cards.map((card) => (
+                      <img className="w-[150px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300" src={card.url} alt='/' />
+                    ))}
+                  </div>
+                  <MdChevronRight className="opacity-50 cursor-pointer hover:opacity-100" size={40} onClick={slideRight} />
+                </div>
+
               </div>
 
-              {/* ------------ The Two Extra Boxes ------------ */}
-              <div className="extras-container">
+              {/* ------------ The Two Extra Boxes ------------
+              {/* <div className="extras-container">
 
-                {/* ------------ The left extra box ------------ */}
+                {/* ------------ The left extra box ------------
                 <div className={inflate ? deflate ? "extra-1-deflate" : "extra-1-expand" : "extra-1"}>
 
                   <div className="extra-content-grid-container">
 
                     <div className="spacer"></div>
-                    {/* ------------ Feels Like information ------------ */}
+                    {/* ------------ Feels Like information ------------
                     <div className="extra-content-container">
                       <img src='temperature-icon.png' className="temperature-icon" alt="temperature-icon" />
                       <div>
@@ -91,20 +229,19 @@ function App() {
                         <h1 className="extra-content-subtitle">50°</h1>
                       </div>
 
-                      {(inflate && !deflate) && (
-                        <div className="extra-content-container-col-2-row-1">
+                      {(fade) && (
+                        <div className={inflate ? deflate ? "extra-content-container-col-2-row-1-fade-out" : "extra-content-container-col-2-row-1-fade-in" : "extra-content-container-col-2-row-1"}>
                           <img src='wind-icon.png' className="wind-icon" alt="wind-icon" />
 
                           <div>
                             <h3 className="extra-content-title-wind">Wind</h3>
                           </div>
                         </div>
-                        
                       )}
                       
                     </div>
 
-                    {/* ------------ Sunrise Information ------------ */}
+                    {/* ------------ Sunrise Information ------------
                     <div className="extra-content-container">
                       <img src='sunrise-icon.png' className="sunrise-sunset-icon" alt="sunrise-icon" />
                       
@@ -113,8 +250,8 @@ function App() {
                         <h1 className="extra-content-subtitle">7:03am</h1>
                       </div>
 
-                      {(inflate && !deflate) && (
-                        <div className="extra-content-container-col-2-row-2">
+                      {(fade) && (
+                        <div className={inflate ? deflate ? "extra-content-container-col-2-row-2-fade-out" : "extra-content-container-col-2-row-2-fade-in" : "extra-content-container-col-2-row-2"}>
                           <h1 className='wind-speed-text'>7</h1>
 
                           <div className="wind-speed-container">
@@ -134,7 +271,7 @@ function App() {
                       
                     </div>
 
-                    {/* ------------ Sunrise Information ------------ */}
+                    {/* ------------ Sunrise Information ------------
                     <div className="extra-content-container">
                       <img src='sunset-icon.png' className="sunrise-sunset-icon" alt="sunrise-icon" />
                       <div>
@@ -142,8 +279,8 @@ function App() {
                         <h1 className="extra-content-subtitle">7:33pm</h1>
                       </div>
 
-                      {(inflate && !deflate) &&(
-                        <div className="extra-content-container-col-2-row-2">
+                      {(fade) &&(
+                        <div className={inflate ? deflate ? "extra-content-container-col-2-row-2-fade-out" : "extra-content-container-col-2-row-2-fade-in" : "extra-content-container-col-2-row-2"}>
                           <h1 className='wind-speed-text'>14</h1>
 
                           <div className="wind-gusts-container">
@@ -161,12 +298,12 @@ function App() {
 
                 </div>
                 
-                {/* ------------ Right extra box ------------ */}
+                {/* ------------ Right extra box ------------
                 <div className={inflate ? deflate ? "extra-2-inflate" : "extra-2-deflate" : "extra-2"}>
                 
                 </div>
                 
-              </div>
+              </div> */}
 
             </div>
 

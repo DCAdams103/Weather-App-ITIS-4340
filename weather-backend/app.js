758e7c146b57e8b08a4e27a2eeb1bde0dc011f10
Dylan Adams <dylancadams1@gmail.com>
2024-06-02 20:18:46 -0400

    Update app.js

--- weather-backend/app.js
+++ weather-backend/app.js
@@ -39,9 +39,10 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  if(err) next(err);
   // render the error page
-  res.status(err.status || 500);
-  res.send(err.message);
+  // res.status(err.status || 500);
+  // res.send(err.message);
   //res.render('error');
 });
 

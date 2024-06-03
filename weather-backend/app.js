fb231bab344a4e3b7912d519520e6fc69b41fd34
Dylan Adams <dylancadams1@gmail.com>
2024-06-02 20:15:08 -0400

    Update app.js
    Got rid of res.render since one online solution mentioned that Vercel doesn't like views

--- weather-backend/app.js
+++ weather-backend/app.js
@@ -41,7 +41,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send(err.message);
+  //res.render('error');
 });
 
 app.listen(process.env.PORT, function() { console.log('Server is running...'); });
